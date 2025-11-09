096c803a6cfb162cbebffe85173352d3a2c78e18
Doma2001 <mohamed.doma2001@gmail.com>
2025-11-09 22:34:11 +0200

    sign in and up files from localhost/api/login to /api/login

--- login_page/src/SignIn.js
+++ login_page/src/SignIn.js
@@ -22,7 +22,7 @@ function SignIn(props) {
     setError("");
 
     try {
-      const response = await fetch("http://backend:5000/api/login", {
+      const response = await fetch("/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
