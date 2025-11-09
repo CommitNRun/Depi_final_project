95d0003bb5918df19deecbf726687bede6c0d59b
Doma2001 <mohamed.doma2001@gmail.com>
2025-11-09 23:36:06 +0200

    express server.js editted and signin and up

--- login_page/src/SignIn.js
+++ login_page/src/SignIn.js
@@ -22,7 +22,7 @@ function SignIn(props) {
     setError("");
 
     try {
-      const response = await fetch("/api/login", {
+      const response = await fetch("/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
