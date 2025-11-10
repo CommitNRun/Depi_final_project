8050fe1042826ce14ed42a7938dcb891a5dea8ae
Doma2001 <mohamed.doma2001@gmail.com>
2025-11-10 02:41:42 +0200

    The webapplication is fully working now

--- login_page/src/App.test.js
+++ login_page/src/App.test.js
@@ -14,8 +14,4 @@ test('renders learn react link', () => {
 });
 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
